a76a4444c0137e9d822a945efc0a1a9850f98c5c
DudeYouth <1157536217@qq.com>
2018-10-25 01:00:53 +0800

    注册页面交互

--- src/components/Common/Register.vue
+++ src/components/Common/Register.vue
@@ -4,25 +4,32 @@
         <h4 class="login-dialog-msg">BOKE您的烂笔头</h4>
         <el-row>
             <el-col :span="18" :offset="3">
-                <div class="login-dialog-top15">
-                    <el-input placeholder="请输入您的邮箱账号" v-model="email">
-                        <template slot="prepend"><i class="el-icon-message"></i></template>
-                        
-                    </el-input>
-                </div>
-                <div class="login-dialog-top15">
-                    <el-input placeholder="请输入验证码" v-model="code">
-                        <template slot="prepend"><i class="el-icon-picture-outline"></i></template>
-                        <template slot="append"><img style="width:50px;" src="../../../static/images/yanzhengma.jpg" /></template>
-                    </el-input>
-                </div>
+                <el-form ref="formData" :rule="formRules" :model="formData">
+                    <div class="login-dialog-top15">
+                        <el-input placeholder="请输入你的手机号码" v-model="formData.mobile">
+                            <template slot="prepend"><i class="el-icon-message"></i></template>
 
+                        </el-input>
+                    </div>
+                    <div class="login-dialog-top15">
+                        <el-input placeholder="请输入您的邮箱账号" v-model="formData.email">
+                            <template slot="prepend"><i class="el-icon-message"></i></template>
+
+                        </el-input>
+                    </div>
+                    <div class="login-dialog-top15">
+                        <el-input placeholder="请输入手机验证码" v-model="formData.code">
+                            <template slot="prepend"><i class="el-icon-picture-outline"></i></template>
+                            <template slot="append"><el-button type="primary" @click="sendCode">发送验证码</el-button></template>
+                        </el-input>
+                    </div>
+                </el-form>
             </el-col>
         </el-row>
         <span slot="footer" class="dialog-footer">
             <el-row>
                 <el-col :span="18" :offset="3">
-                    <el-button type="primary" style="width:100%" @click="closeDialog">注册</el-button>
+                    <el-button type="primary" style="width:100%" @click="registerSubmit">注册</el-button>
                     <p class="login-dialog-tip">注册即代表同意《BOKE协议》《隐私政策》</p>
                 </el-col>
             </el-row>
@@ -30,48 +37,90 @@
     </el-dialog>
 </template>
 <script>
-    export default {
-        name: 'RegisterDialog',
-        props:['dialogVisible'],
-        data() {
-            return {
-                email:'',
-                code:'',
-            }
-            
-        },
-        methods: {
-            closeDialog:function(){
-                this.$emit('close');
-            },
-            close:function(){
-                this.$emit('close');
+export default {
+    name: "RegisterDialog",
+    props: ["dialogVisible"],
+    data() {
+        const checkMoblie = (rule, value,callback)=>{
+            if( !/^1[35678][0-9]{9}$/.test(value) ){
+                callback(new Error('请输入正确的手机号码！'));
+            }else{
+                callback();
             }
         }
+        return {
+            formData: {
+                mobile: "",
+                email: "",
+                code: ""
+            },
+            formRules:{
+                mobile:[
+                    {
+                        require:true,
+                        message:"请输入您的手机号码",
+                        trigger:'blur'
+                    },
+                    {
+                        validator: checkMoblie,  
+                        trigger:'blur'
+                    },
+                ]
+            }
+        };
+    },
+    methods: {
+        sendCode() {},
+        registerSubmit() {
+            this.$refs["formData"].validate((valid) => {
+                if (valid) {
+                    this.$axios.post('/user/register',this.formData).then(res=>{
+                        if( res.status===100 ){
+                            this.$message({
+                                message: '注册成功！',
+                                type: 'success'
+                            });
+                        }else{
+                            this.$message({
+                                message: res.content,
+                                type: 'error'
+                            });
+                        }
+                    })
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        close() {
+            this.$emit("close");
+        }
     }
+};
 </script>
 <style>
-.el-dialog__header{
-    padding:0px!important;
-}
-.login-dialog-msg,.login-dialog-title{
-    text-align:center;
-}
-.login-dialog-title{
-    text-align:center;
-    color:#0084ff;
-}
-.login-dialog-top15{
-    margin-top:15px;
-}
-.login-dialog-top30{
-    margin-top:30px;
-}
-.login-dialog-tip{
-    color:#8590a6;
-    margin:10px 0;
-    text-align:left;
-    font-size:14px; 
-}
-
+    .el-dialog__header {
+        padding: 0px !important;
+    }
+    .login-dialog-msg,
+    .login-dialog-title {
+        text-align: center;
+    }
+    .login-dialog-title {
+        text-align: center;
+        color: #0084ff;
+    }
+    .login-dialog-top15 {
+        margin-top: 15px;
+    }
+    .login-dialog-top30 {
+        margin-top: 30px;
+    }
+    .login-dialog-tip {
+        color: #8590a6;
+        margin: 10px 0;
+        text-align: left;
+        font-size: 14px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,21 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '../static/css/common.css'
 import App from './App'
 import router from './router'
+import axios from "axios"
 
+axios.defaults.baseURL = 'http://www.ricky.kim/server';
+axios.defaults.headers.common['Content-Type'] = "application/json";
+axios.interceptors.response.use((response) => {
+    return response.data;
+}, function(error) {
+    return Promise.reject(error);
+});
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-    /* eslint-disable no-new */
+
+/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
